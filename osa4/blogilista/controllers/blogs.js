848292b504b912168cf918d356e9596d68acab47
Raine Rantanen <raine.ran7anen@gmail.com>
2020-03-29 17:00:28 +0300

    4.12 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -8,8 +8,8 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
-  const result = await blog.save();
-  response.status(201).json(result.toJSON());
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog.toJSON());
 });
 
 module.exports = blogsRouter;
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -3,31 +3,12 @@ const supertest = require('supertest');
 const app = require('../app');
 const api = supertest(app);
 const Blog = require('../models/blog');
-
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5
-  }
-];
+const helper = require('./test_helper');
 
 describe('In blogs API', () => {
   beforeEach(async () => {
     await Blog.deleteMany({});
-
-    let blogObject = new Blog(initialBlogs[0]);
-    await blogObject.save();
-
-    blogObject = new Blog(initialBlogs[1]);
-    await blogObject.save();
+    await Blog.insertMany(helper.initialBlogs);
   });
 
   describe('using GET', () => {
@@ -40,7 +21,7 @@ describe('In blogs API', () => {
 
     test('there are two blogs', async () => {
       const response = await api.get('/api/blogs');
-      expect(response.body.length).toBe(initialBlogs.length);
+      expect(response.body.length).toBe(helper.initialBlogs.length);
     });
 
     test('there is an id property defined', async () => {
@@ -64,11 +45,10 @@ describe('In blogs API', () => {
         .expect(201)
         .expect('Content-Type', /application\/json/);
 
-      const response = await api.get('/api/blogs');
+      const allBlogs = await helper.blogsInDb();
+      const titles = allBlogs.map(r => r.title);
 
-      const titles = response.body.map(r => r.title);
-
-      expect(response.body.length).toBe(initialBlogs.length + 1);
+      expect(allBlogs.length).toBe(helper.initialBlogs.length + 1);
       expect(titles).toContain(newBlog.title);
     });
 
@@ -86,6 +66,32 @@ describe('In blogs API', () => {
         .expect('Content-Type', /application\/json/);
 
       expect(response.body.likes).toBe(0);
+    });
+
+    test('New blog entry must contain value for title', async () => {
+      const newBlog = {
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5
+      };
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400);
+    });
+
+    test('New blog entry must contain value for url', async () => {
+      const newBlog = {
+        title: 'New Blog Entry',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      };
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400);
     });
   });
 
