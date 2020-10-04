e3fc016c157206d5883ebc9a6aaa13ffc8b8d918
Raine Rantanen <raine.ran7anen@gmail.com>
2020-10-04 15:31:20 +0300

    5.7 done.

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const [displayFull, setDisplayFull] = useState(false);
+
+  const { title, author, url, likes, user } = blog;
+
+  const toggleView = () => setDisplayFull(!displayFull);
+
+  const displayButton = <button onClick={toggleView}>{displayFull ? 'hide' : 'view'}</button>;
+
+  const likeButton = <button>like</button>;
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {title} {author} {displayButton}
+      </div>
+      {displayFull && (
+        <div>
+          <p>{url}</p>
+          <p>
+            {likes} {likeButton}
+          </p>
+          <p>{user && user.username}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
